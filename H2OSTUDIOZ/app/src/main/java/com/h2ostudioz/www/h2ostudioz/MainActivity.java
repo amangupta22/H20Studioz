f8e85d1d8b2d6f2844a031cf3b8edbf0f53b78fe
Aman Gupta <aman.work95@gmail.com>
2016-03-20 08:29:25 +0530

    android code for H2OStudioz
    ........................

---
+++ H2OSTUDIOZ/app/src/main/java/com/h2ostudioz/www/h2ostudioz/MainActivity.java
@@ -0,0 +1,19 @@
+package com.h2ostudioz.www.h2ostudioz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
